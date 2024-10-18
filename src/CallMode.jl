f18a09bd9db10f966164aa4cdd0d302dfff6f40f
AntonOresten <anton.oresten42@gmail.com>
2024-10-19 00:46:03 +0200

    Minimize parenthesis usage in source

--- src/CallMode.jl
+++ src/CallMode.jl
@@ -3,30 +3,13 @@ module CallMode
 export @call
 
 using ReplMaker
+using Setfield
 
 macro call(func, args...)
-    positional_args = []
-    keyword_args = []
-    
-    for arg in args
-        if isa(arg, Expr) && arg.head == :(=)
-            push!(keyword_args, Expr(:kw, arg.args[1], arg.args[2]))
-        else
-            push!(positional_args, arg)
-        end
-    end
-    
-    return :($(esc(func))($(map(esc, positional_args)...); $(map(esc, keyword_args)...)))
+    quote $(func |> esc)($([arg isa Expr && arg.head == :(=) ? begin @set arg.head = :kw end |> esc : arg |> esc for arg in args]...)) end
 end
 
-function init_call_mode()
-    initrepl(input -> Meta.parse("CallMode.@call $input"), # only flaw of this package. `CallMode` isn't necessarily accessible
-             prompt_text="@call> ",
-             prompt_color = :magenta,
-             start_key = ')',
-             mode_name = "Call Mode")
-end
-
-__init__() = isdefined(Base, :active_repl) && init_call_mode()
+__init__() = begin @eval Base @isdefined active_repl end &&
+    @call initrepl input -> "@call $input" |> Meta.parse prompt_text="@call> " prompt_color=:magenta start_key=')' mode_name="Call Mode"
 
 end

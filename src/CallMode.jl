b61a0e6a565f3aefe090c50d56c4c47839d4124a
AntonOresten <anton.oresten42@gmail.com>
2024-10-31 01:20:31 +0100

    Refactor

--- src/CallMode.jl
+++ src/CallMode.jl
@@ -1,25 +1,25 @@
 module CallMode
 
-export @call
-
 using ReplMaker
 using Setfield
 
-call = arguments -> begin
-    _, _, func, args... = arguments
-    begin
-        :call, func |> esc,
-        begin
-            begin
-                :call, map,
-                arg -> arg isa Expr && arg.head == "=" |> Symbol ? begin @set arg.head = :kw end |> esc : arg |> esc,
-                args
-            end |> begin Expr |> splat end |> eval
-        end...
-    end |> begin Expr |> splat end
-end
+export @call
 
-var"@call" = call ∘ tuple
+var"@call" = begin
+    arguments -> begin
+        _, _, func, args... = arguments
+        begin
+            :call, func |> esc,
+            begin
+                begin
+                    :call, map,
+                    arg -> arg isa Expr && arg.head == "=" |> Symbol ? begin @set arg.head = :kw end |> esc : arg |> esc,
+                    args
+                end |> begin Expr |> splat end |> eval
+            end...
+        end |> begin Expr |> splat end
+    end
+end ∘ tuple
 
 __init__ = begin end -> begin @eval Base @isdefined active_repl end &&
     @call initrepl input -> "@call $input" |> Meta.parse prompt_text="@call> " prompt_color=:magenta start_key=')' mode_name="Call Mode"

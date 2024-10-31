df11be56a18aeafa1b8501e8faae0532ea5135bc
AntonOresten <anton.oresten42@gmail.com>
2024-10-31 01:10:14 +0100

    Remove all parentheses from code

--- src/CallMode.jl
+++ src/CallMode.jl
@@ -5,11 +5,23 @@ export @call
 using ReplMaker
 using Setfield
 
-macro call(func, args...)
-    quote $(func |> esc)($([arg isa Expr && arg.head == :(=) ? begin @set arg.head = :kw end |> esc : arg |> esc for arg in args]...)) end
+call = arguments -> begin
+    _, _, func, args... = arguments
+    begin
+        :call, func |> esc,
+        begin
+            begin
+                :call, map,
+                arg -> arg isa Expr && arg.head == "=" |> Symbol ? begin @set arg.head = :kw end |> esc : arg |> esc,
+                args
+            end |> begin Expr |> splat end |> eval
+        end...
+    end |> begin Expr |> splat end
 end
 
-__init__() = begin @eval Base @isdefined active_repl end &&
+var"@call" = call ∘ tuple
+
+__init__ = begin end -> begin @eval Base @isdefined active_repl end &&
     @call initrepl input -> "@call $input" |> Meta.parse prompt_text="@call> " prompt_color=:magenta start_key=')' mode_name="Call Mode"
 
 end
